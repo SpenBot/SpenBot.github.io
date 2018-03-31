5582024fe91346d96d6d3dfbd891a9962f56d668
Spenser Holstein <spenser.holstein@gmail.com>
2018-03-31 16:34:29 -0400

    added yum, and change some styling

--- js/script.js
+++ js/script.js
@@ -7,14 +7,17 @@ let videoLink = document.getElementById('videoLink')
 if (window.location.href.substring(window.location.href.length - 10) == 'index.html') {
   homeLink.style.textDecoration = "underline"
   homeLink.style.fontWeight = "bolder"
-  homeLink.style.color = "gray"
+  homeLink.style.color = "rgb(0, 16, 40)"
+  homeLink.style.backgroundColor = "white"
 
 } else if (window.location.href.substring(window.location.href.length - 13) == 'moreWork.html') {
   moreWorkLink.style.textDecoration = "underline"
   moreWorkLink.style.fontWeight = "bolder"
-  moreWorkLink.style.color = "gray"
+  moreWorkLink.style.color = "rgb(0, 16, 40)"
+  moreWorkLink.style.backgroundColor = "white"
 } else if (window.location.href.substring(window.location.href.length - 10) == 'video.html') {
   videoLink.style.textDecoration = "underline"
   videoLink.style.fontWeight = "bolder"
-  videoLink.style.color = "gray"
+  videoLink.style.color = "rgb(0, 16, 40)"
+  videoLink.style.backgroundColor = "white"
 }
